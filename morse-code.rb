a2c40a1c6ea7fb97c8bcff12f3a8ef6f8334d46a
Momo-87 <momochristian87@gmail.com>
2023-07-19 14:53:39 +0100

    Create a method to decode an entire word in Morse code

--- morse-code.rb
+++ morse-code.rb
@@ -5,6 +5,11 @@ MORSE_CODE = {'.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E
 '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
 '--..' => 'Z'}
 
-def decode_morse_characters(morse_code)
-    MORSE_CODE[morse_code] || ""
+def decode_morse_char(morse_char)
+    MORSE_CODE[morse_char] || ""
+end
+
+def decode_morse_word(morse_word)
+    morse_chars = morse_word.split
+    morse_chars.map { |char| decode_morse_char(char) }.join
 end
